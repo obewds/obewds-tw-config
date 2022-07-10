fca1324b11f3209e9581f0d6f5a0cb8008871445
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-09 23:40:19 -0400

    removed @obewds/vue-component-helpers from main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
 
-import { mergeAppConfigWithDefaults } from '@obewds/vue-component-helpers'
-import appTwConfig from '../obewds.tw.config.json'
-
 const app = createApp(App)
-
-const tw = mergeAppConfigWithDefaults(appTwConfig)
-
-// app.provide('tw', tw)
 
 app.provide('globals', {
     classes: {
